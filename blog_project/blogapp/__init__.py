c9e7944ac032bcc050520477696875c032b2847a
smdas989 <smdas989@gmail.com>
2021-04-27 19:37:21 +0530

    profile updation

--- blog_project/blogapp/__init__.py
+++ blog_project/blogapp/__init__.py
@@ -12,8 +12,9 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
-# login_manager.login_view = 'login'
-# login_manager.login_message_category = 'info'
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from blogapp import views
 from blogapp.models import *
