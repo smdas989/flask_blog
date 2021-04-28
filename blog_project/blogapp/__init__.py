a1940e9e00f02b5c7498b40552994720a1104da9
smdas989 <smdas989@gmail.com>
2021-04-28 19:04:50 +0530

    reset password

--- blog_project/blogapp/__init__.py
+++ blog_project/blogapp/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a7314aa1b604963cd35ed968e6b0c00d'
@@ -15,6 +17,12 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from blogapp import views
 from blogapp.models import *
