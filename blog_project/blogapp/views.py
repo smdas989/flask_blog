ae8618aec0cc94244f971465b5287649d109713e
smdas989 <smdas989@gmail.com>
2021-04-28 17:34:08 +0530

    pagination

--- blog_project/blogapp/views.py
+++ blog_project/blogapp/views.py
@@ -8,11 +8,15 @@ from flask_login import login_user, current_user, logout_user, login_required
 import secrets
 import os
 from PIL import Image
+from flask_paginate import Pagination, get_page_parameter
 
 @app.route("/")
+
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    # posts = Post.query.paginate(per_page=5, page=page_num, error_out=True)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
